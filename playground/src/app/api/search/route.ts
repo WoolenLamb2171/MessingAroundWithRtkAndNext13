f2416a77f88adcda45673196488e79de1ea1abec
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-08-13 13:52:05 +0300

    fix api

--- playground/src/app/api/search/route.ts
+++ playground/src/app/api/search/route.ts
@@ -1,13 +1,15 @@
 import { NextResponse } from "next/server";
-import pokemon from "@/pokemon.json";
+import { pokemon } from "@/pokemon";
 
 // /api/search/?name=THE_NAME
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const name = searchParams.get("name");
-    const pokemonData = pokemon.filter((p) => {
-        p.name.toLocaleLowerCase().includes(name?.toLocaleLowerCase() ?? "");
-    });
+    // const pokemonData = pokemon;
+    const pokemonData = pokemon.filter((p) =>
+        p.name.toLowerCase().includes(name?.toLowerCase() ?? "")
+    );
+
     // to not overhelm use .slice(0, 10)
     return NextResponse.json(pokemonData.slice(0, 10));
 }
--- playground/src/app/page.tsx
+++ playground/src/app/page.tsx
@@ -1,9 +1,11 @@
+//for server side only components we can use async funtions
 
-
-export default function Home() {
+export default async function Home() {
+  const req = await fetch("http://localhost:3000/api/search");
+  const data = await req.json();
   return (
     <main className="flex min-h-screen flex-col">
-
+      {JSON.stringify(data)}
     </main>
   )
 }
