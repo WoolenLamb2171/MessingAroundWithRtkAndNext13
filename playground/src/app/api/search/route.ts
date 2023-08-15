4b7a3cc752d3577d15b590756d07823bfe235852
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-08-15 18:54:48 +0300

    fix fetching

--- playground/src/app/api/search/route.ts
+++ playground/src/app/api/search/route.ts
@@ -1,15 +1,13 @@
 import { NextResponse } from "next/server";
-import { pokemon } from "@/pokemon";
+import pokemon from "@/pokemon.json"
 
 // /api/search/?name=THE_NAME
+
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const name = searchParams.get("name");
-    // const pokemonData = pokemon;
-    const pokemonData = pokemon.filter((p) =>
-        p.name.toLowerCase().includes(name?.toLowerCase() ?? "")
-    );
-
-    // to not overhelm use .slice(0, 10)
-    return NextResponse.json(pokemonData.slice(0, 10));
+    const pokemonData = pokemon.filter(pokemon => 
+        pokemon.name.toLowerCase().includes(name?.toLowerCase() ?? "")
+    ).slice(0, 10)  
+    return NextResponse.json(pokemonData)
 }
--- playground/src/app/page.tsx
+++ playground/src/app/page.tsx
@@ -1,8 +1,12 @@
 //for server side only components we can use async funtions
 
 export default async function Home() {
-  const req = await fetch("http://localhost:3000/api/search");
+  const req = await fetch("http://localhost:3000/api/search", {
+    next: { revalidate: 10 },
+  });
+
   const data = await req.json();
+
   return (
     <main className="flex min-h-screen flex-col">
       {JSON.stringify(data)}
